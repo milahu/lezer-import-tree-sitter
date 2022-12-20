ecb1a17f3e670d573aa8106ba7bd267777d25578
Milan Hauth <milahu@gmail.com>
2022-12-20 21:00:52 +0100

    update test

--- test/cases/tree-sitter-cpp/out/actual/scanner.js
+++ test/cases/tree-sitter-cpp/out/actual/scanner.js
@@ -1,115 +1,101 @@
 // scanner.js - generated from scanner.cc
-
 import { ExternalTokenizer } from "@lezer/lr";
-
 // @ts-ignore Cannot find module - file is generated
 import * as Tokens from "./parser.terms.js";
-
 // ascii chars
-const smallU = 117,
-  doubleQuotes = 34,
-  parenOpen = 40,
-  parenClose = 41,
+const doubleQuotes = 34,
+  parenL = 40,
+  parenR = 41,
   number8 = 56,
   bigL = 76,
   bigR = 82,
   bigU = 85,
-  backslash = 92;
-
+  backslash = 92,
+  smallU = 117;
 const spaceCodeSet = new Set([
   9, 10, 11, 12, 13, 32, 133, 160, 5760, 8192, 8193, 8194, 8195, 8196, 8197,
   8198, 8199, 8200, 8201, 8202, 8232, 8233, 8239, 8287, 12288,
 ]);
-
-/** @param {number} code */
-const iswspace = (code) => spaceCodeSet.has(code);
-
+/** @param {number} code */ const iswspace = (code) => spaceCodeSet.has(code);
+/** @param {number} code */ const /* eslint: no-unused-vars: 'iswdigit' is assigned a value but never used. */ iswdigit =
+    (code) => 48 <= code && code <= 57;
+/** @param {number} code */ const /* eslint: no-unused-vars: 'iswalpha' is assigned a value but never used. */ iswalpha =
+    (code) => 65 <= code && code <= 122 && (code <= 90 || 97 <= code);
 export const RawStringLiteral = new ExternalTokenizer((input) => {
-  /// workaround for https://github.com/microsoft/TypeScript/issues/9998
-  const inputNext = () => /** @type {number} */ input.next;
-  while (iswspace(inputNext())) {
+  while (iswspace(input.next)) {
     /// TODO skip whitespace
     /// original call:
     /// lexer->advance(lexer, true)
     input.advance();
   }
-  /// TODO defer acceptToken?
   input.acceptToken(Tokens.RawStringLiteral);
-
   // Raw string literals can start with: R, LR, uR, UR, u8R
   // Consume 'R'
-  if (inputNext() == bigL || inputNext() == bigU) {
+  if (input.next == bigL || input.next == bigU) {
     input.advance();
-    if (inputNext() != bigR) {
+    if (input.next != bigR) {
       /// TODO return?
       return false;
     }
-  } else if (inputNext() == smallU) {
+  } else if (input.next == smallU) {
     input.advance();
-    if (inputNext() == number8) {
+    if (input.next == number8) {
       input.advance();
-      if (inputNext() != bigR) {
+      if (input.next != bigR) {
         /// TODO return?
         return false;
       }
-    } else if (inputNext() != bigR) {
+    } else if (input.next != bigR) {
       /// TODO return?
       return false;
     }
-  } else if (inputNext() != bigR) {
+  } else if (input.next != bigR) {
     /// TODO return?
     return false;
   }
   input.advance();
   // Consume '"'
-  if (inputNext() != doubleQuotes) {
+  if (input.next != doubleQuotes) {
     /// TODO return?
     return false;
   }
   input.advance();
   // Consume '(', delimiter
-  /** @type {number[]} */
-  const delimiter = [];
+  /** @type {number[]} */ const delimiter = [];
   while (true) {
-    if (inputNext() == 0 || inputNext() == backslash || iswspace(inputNext())) {
+    if (0 == input.next || input.next == backslash || iswspace(input.next)) {
       /// TODO return?
       return false;
     }
-    if (inputNext() == parenOpen) {
+    if (input.next == parenL) {
       input.advance();
       break;
     } /// converted string to number[]
-    delimiter.push(inputNext());
+    delimiter.push(input.next);
     input.advance();
   }
   // Consume content, delimiter, ')', '"'
-  /** @type {number} */
-  let delimiter_index = -1;
+  /** @type {number} */ let delimiter_index = -1;
   while (true) {
-    if (inputNext() == 0) {
+    if (0 == input.next) {
       /// TODO return?
       return false;
     }
     if (delimiter_index >= 0) {
       if (delimiter_index == delimiter.length) {
-        if (inputNext() == doubleQuotes) {
+        if (input.next == doubleQuotes) {
           input.advance();
           /// TODO return?
           return true;
-        } else {
-          delimiter_index = -1;
         }
+        delimiter_index = -1;
       } else {
-        if (inputNext() == delimiter[delimiter_index]) {
-          delimiter_index++;
-        } else {
-          delimiter_index = -1;
-        }
+        input.next == delimiter[delimiter_index]
+          ? delimiter_index++
+          : (delimiter_index = -1);
       }
     }
-    if (delimiter_index == -1 && inputNext() == parenClose) {
-      delimiter_index = 0;
-    }
+    -1 == delimiter_index && input.next == parenR && (delimiter_index = 0);
     input.advance();
   }
 });
