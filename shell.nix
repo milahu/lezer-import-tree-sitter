082d82479516314034d5040e0cb18332ab86bb9a
Milan Hauth <milahu@gmail.com>
2022-12-21 15:49:23 +0100

    downgrade to node 16 for node-tree-sitter

--- shell.nix
+++ shell.nix
@@ -2,13 +2,25 @@
 
 with pkgs;
 
+# node-tree-sitter requires node 16
+# https://github.com/tree-sitter/node-tree-sitter/issues/106
+
+let
+#_nodejs = nodejs-19_x; # error
+#_nodejs = nodejs-18_x; # error
+#_nodejs = nodejs-17_x; # not in nixpkgs
+_nodejs = nodejs-16_x;
+in
+
 mkShell {
 
 buildInputs = [
 gnumake # make
+gcc
 python3 # for node-gyp
 tree-sitter
-nodejs.pkgs.node-gyp
+_nodejs
+_nodejs.pkgs.node-gyp
 ];
 
 }
