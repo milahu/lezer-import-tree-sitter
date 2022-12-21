5007e6edf18bea858d3a46c1df510c3fc4716244
Milan Hauth <milahu@gmail.com>
2022-12-21 17:57:54 +0100

    up devstep.sh

--- devstep.sh
+++ devstep.sh
@@ -4,6 +4,8 @@ node src/import-scanner.js \
 test/cases/tree-sitter-cpp/src/scanner.cc \
 test/cases/tree-sitter-cpp/src/grammar.json
 
-node src/import-scanner.js \
+false && node src/import-scanner.js \
 test/cases/tree-sitter-bash/src/scanner.cc \
 test/cases/tree-sitter-bash/src/grammar.json
+
+( cd test/cases/bash/lezer-parser-bash/; npm run build 2>lezer-generator.err.txt )
