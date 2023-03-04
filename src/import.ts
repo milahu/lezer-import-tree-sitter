eb00c59f35247328af57024ba992eb4c3af42d69
Milan Hauth <milahu@gmail.com>
2023-03-04 14:41:39 +0100

    pretty external tokens

--- src/import.ts
+++ src/import.ts
@@ -306,8 +306,8 @@ export class Context {
     let ruleStr = rules.map(r => `${r} ${this.rules[r]}\n\n`).join("")
     let externalStr = ""
     if (this.def.externals && this.def.externals.length) {
-      const tmp = this.def.externals.filter((s): s is SymbolExpr => s.type == "SYMBOL").map(s => this.translateName(s.name)).join(", ")
-      externalStr = `@external tokens token from "./scanner.js" { ${tmp} }\n\n`
+      const tmp = this.def.externals.filter((s): s is SymbolExpr => s.type == "SYMBOL").map(s => this.translateName(s.name)).join(",\n  ")
+      externalStr = `@external tokens token from "./scanner.js" {\n  ${tmp}\n}\n\n`
     }
     let tokens = Object.keys(this.tokens)
     let tokenStr = `@tokens {\n${tokens.map(t => `  ${t} ${this.tokens[t]}\n`).join("")}}`
