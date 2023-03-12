b1b67f99a29d29a7becbec364f93e1d781f6dcef
Milan Hauth <milahu@gmail.com>
2023-03-12 17:15:36 +0100

    pretty test runner

--- test/import-antlr4/test.sh
+++ test/import-antlr4/test.sh
@@ -23,7 +23,11 @@ d="$dir/lezer-parser-$name/src"
 parser=$(ls $dir/antlr4-grammar-$name/*Parser.g4)
 lexer=$(ls $dir/antlr4-grammar-$name/*Lexer.g4)
 out="$dir/lezer-parser-$name/src/grammar.lezer"
-echo "importing to $out"
+echo "importing from"
+echo "  $parser"
+echo "  $lexer"
+echo "to"
+echo "  $out"
 echo "node src/import-antlr4 $parser $lexer >$out"
 node src/import-antlr4 $parser $lexer >$out
 
